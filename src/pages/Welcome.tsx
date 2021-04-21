ad07523c6fb8eaf72a46a53e4acd9d04a50fe8a8
Lucas Delbel <lucdelbel@gmail.com>
2021-04-21 10:17:35 -0300

    Refactor welcome page

---
+++ src/pages/Welcome.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+import {
+  SafeAreaView,
+  Text,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  Dimensions,
+  View,
+} from "react-native";
+import { Entypo } from "@expo/vector-icons";
+import wateringImg from "../assets/watering.png";
+import colors from "../styles/colors";
+import fonts from "../styles/fonts";
+
+export function Welcome() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.wrapper}>
+        <Text style={styles.title}>Manage {"\n"} your plants easily</Text>
+
+        <Image source={wateringImg} style={styles.image} resizeMode="contain" />
+
+        <Text style={styles.subtitle}>
+          Don't forget to water your plants anymore. We take care to remember
+          you whenever you need.
+        </Text>
+
+        <TouchableOpacity style={styles.button} activeOpacity={0.7}>
+          <Text>
+            <Entypo name="chevron-right" style={styles.buttonIcon} />
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  wrapper: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-around",
+    paddingHorizontal: 20,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: colors.heading,
+    marginTop: 38,
+    fontFamily: fonts.heading,
+    lineHeight: 38,
+  },
+  subtitle: {
+    textAlign: "center",
+    fontSize: 18,
+    paddingHorizontal: 20,
+    color: colors.heading,
+    fontFamily: fonts.text,
+  },
+  image: {
+    height: Dimensions.get("window").width * 0.7,
+  },
+  button: {
+    backgroundColor: colors.green,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 16,
+    marginBottom: 10,
+    height: 56,
+    width: 56,
+  },
+  buttonIcon: {
+    color: colors.white,
+    fontSize: 28,
+  },
+});
